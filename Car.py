1743127a65e3f9a55e377f028a007afc5886e59f
Vladvanins <73720539+Vladvanins@users.noreply.github.com>
2021-06-16 15:20:29 +0300

    Add files via upload

---
+++ Car.py
@@ -0,0 +1,35 @@
+class Car:
+
+    def __init__(self, name, color, speed, is_police=False):
+        self.name= name
+        self.color = color
+        self.speed = speed
+        self.is_police = is_police
+        print(f'Новая машина: {self.name} (цвет {self.color}) машина полицейская - {self.is_police}')
+
+    def go(self):
+        print(f'{self.name}: Машина поехала.')
+
+    def stop(self):
+        print(f'{self.name}: Машина остановилась.')
+
+    def turn(self, direction):
+        print(f'{self.name}: Машина повернула {"налево" if direction == 0 else "направо"}.')
+
+    def show_speed(self):
+        return f'{self.name} : Скорость автомобиля: {self.speed}.'
+
+class TownCar(Car):
+
+    def show_speed(self):
+        return f'{self.name}: Скорость автомобиля: {self.speed}, Превыение скорости' \
+            if self.speed > 60 else f"{self.name}: Скорость автомобиля {self.speed}"
+
+class WorkCar(Car):
+    def show_speed(self):
+        return f'{self.name}: Скорее автомобиля: {self.speed}. Превышение скорости!' \
+            if self.speed > 40 else f"{self.name}: Скорость автомобиля {self.speed}"
+
+class SportCar(Car):
+
+class PoliceCar(Car):
